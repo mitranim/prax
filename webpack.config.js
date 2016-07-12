4790f5a076baeff7459fa4496b798442e7f773b8
Nelo Mitranim <me@mitranim.com>
2016-07-12 18:21:53 +0300

    `ReactiveClass` now calls the original `componentWillMount`

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
   ] : [
     new webpack.optimize.UglifyJsPlugin({
       minimize: true,
-      compress: {screw_ie8: true},
+      compress: {warnings: false, screw_ie8: true},
       mangle: true
     })
   ],
