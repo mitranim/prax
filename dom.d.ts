2cb9a5386b9c5ac54ac4167e9862ad3a2b7b7b40
Dmitriy Pleshevskiy <dmitriy@ideascup.me>
2021-07-15 14:47:11 +0300

    refac: fix feedback

--- dom.d.ts
+++ dom.d.ts
@@ -1,31 +1,33 @@
 type Nil = null | undefined;
-type Primitive = string | number | bigint | boolean;
+type NonNilPrim = string | number | bigint | boolean;
+type Prim = Nil | NonNilPrim;
 
-type Prop = Nil | Primitive;
-type Props = Nil | Record<string, Prop>;
+interface Stringable {
+    [Symbol.toStringTag](): string
+}
 
-type Child = Nil | Primitive | Node | Child[];
+type Props = Nil | Record<string, NonNilPrim | Stringable>;
+
+type Child = Prim | Node | Child[];
 
 export function E<K extends keyof HTMLElementTagNameMap>(name: K, props?: Props, ...children: Child[]): HTMLElementTagNameMap[K];
 export function E<N extends HTMLElement>(name: string, props?: Props, ...children: Child[]): N;
 
 export function S<K extends keyof SVGElementTagNameMap>(name: K, props?: Props, ...children: Child[]): SVGElementTagNameMap[K];
 
-export function F(...children: Child[]): Node;
+export function F(...children: Child[]): DocumentFragment;
 
-export function reset<N extends Node>(node: N | Nil, props?: Props, ...children: Child[]): N | Nil;
+export function reset<N extends Node>(node: N, props?: Props, ...children: Child[]): N;
 
-export function resetProps<N extends Node>(node: N | Nil, props?: Props): N | Nil;
+export function resetProps<N extends Node>(node: N, props?: Props): N;
 
-export function replace<N extends Node>(node: N | Nil, ...children: Child[]): void;
+export function replace<N extends Node>(node: N, ...children: Child[]): void;
 
-type ClsVal = Nil | Primitive | Array<ClsVal>;
+type ClsVal = Nil | false | 0 | string | Array<ClsVal>;
 
 export function cls(...vals: ClsVal[]): string;
 
-type LenVal = Nil | Primitive | Node | Array<LenVal>;
-
-export function len(val: LenVal): number;
+export function len(val: Child): number;
 
 // TODO
 // export function map(val, fun, ...args): unknown
