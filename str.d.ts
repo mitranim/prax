fa7637651eace89aa5f86ebd67dc2de28f3895ea
Dmitriy Pleshevskiy <dmitriy@ideascup.me>
2021-07-15 17:27:31 +0300

    fix: Raw instead string for str

--- str.d.ts
+++ str.d.ts
@@ -1,14 +1,14 @@
-import type {Props} from './dom.d.ts';
+import type {Props, Raw} from './dom.d.ts';
 
 export {Raw, boolAttrs, voidElems, cls, len, map} from './dom.d.ts';
 
 type Child = string | Child[];
 
-export function E(name: string, props: Props, ...children: Child[]): string;
+export function E(name: string, props: Props, ...children: Child[]): Raw;
 
-export function S(name: string, props: Props, ...children: Child[]): string;
+export function S(name: string, props: Props, ...children: Child[]): Raw;
 
-export function F(...children: Child[]): string;
+export function F(...children: Child[]): Raw;
 
 export function escapeText(val: string): string;
 
